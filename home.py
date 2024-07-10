e154d4d69231d5fdb6d46599130b403840c7eaae
Chhavi <chhavipareek007@gmail.com>
2024-07-10 18:55:29 +0530

    more status codes

--- home.py
+++ home.py
@@ -9,7 +9,28 @@ def homeScreen():
 @app.route("/200")
 def code200():
     filename="images/200statusCode.jpeg"
-    return send_file(filename, mimetype='image')
+    return [send_file(filename, mimetype='image')]
+
+@app.route("/102")
+def code102():
+    filename="images/102statusCode.jpeg"
+    return [send_file(filename, mimetype='image')]
+
+@app.route("/202")
+def code202():
+    filename="images/202statusCode.jpeg"
+    return [send_file(filename, mimetype='image')]
+
+@app.route("/208")
+def code208():
+    filename="images/208statusCode.jpeg"
+    return [send_file(filename, mimetype='image')]
+
+@app.route("/300")
+def code300():
+    filename="images/300statusCode.jpeg"
+    return [send_file(filename, mimetype='image')]
+
 
 if __name__=="__main__":
     app.run()
