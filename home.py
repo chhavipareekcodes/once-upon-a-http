6f0313e791840011e860b2b6ce6d34b63e341cba
Chhavi <chhavipareek007@gmail.com>
2024-07-10 21:25:56 +0530

    dynamic routing

--- home.py
+++ home.py
@@ -6,30 +6,10 @@ app=Flask(__name__)
 def homeScreen():
     return render_template("index.html")
 
-@app.route("/200")
-def code200():
-    filename="images/200statusCode.jpeg"
-    return [send_file(filename, mimetype='image')]
-
-@app.route("/102")
-def code102():
-    filename="images/102statusCode.jpeg"
-    return [send_file(filename, mimetype='image')]
-
-@app.route("/202")
-def code202():
-    filename="images/202statusCode.jpeg"
-    return [send_file(filename, mimetype='image')]
-
-@app.route("/208")
-def code208():
-    filename="images/208statusCode.jpeg"
-    return [send_file(filename, mimetype='image')]
-
-@app.route("/300")
-def code300():
-    filename="images/300statusCode.jpeg"
-    return [send_file(filename, mimetype='image')]
+@app.route("/<code>")
+def satusCode(code):
+    filename=f"images/{code}statusCode.jpeg"
+    return send_file(filename, mimetype='image/jpeg')
 
 
 if __name__=="__main__":
